92304a5ba309c236dc6762c30c29a125814c02cc
Luiz Guilherme <luizgmelo64@gmail.com>
2025-10-02 11:06:51 -0300

    feat: create user controller

---
+++ src/main/java/com/luizgmelo/expense_tracker/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.luizgmelo.expense_tracker.controllers;
+
+import com.luizgmelo.expense_tracker.dto.RegisterDto;
+import com.luizgmelo.expense_tracker.dto.UserDto;
+import com.luizgmelo.expense_tracker.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> register(@RequestBody @Valid RegisterDto registerDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerDto));
+    }
+}
