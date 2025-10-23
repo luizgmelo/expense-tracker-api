b72ab974fd53a753b8fff23c0e9ff04df931b209
Luiz Guilherme <luizgmelo64@gmail.com>
2025-10-22 21:18:26 -0300

    fix: add uuid expenseDto

--- src/main/java/com/luizgmelo/expense_tracker/dto/ExpenseDto.java
+++ src/main/java/com/luizgmelo/expense_tracker/dto/ExpenseDto.java
@@ -1,12 +1,12 @@
 package com.luizgmelo.expense_tracker.dto;
 
 import com.luizgmelo.expense_tracker.models.Expense;
-import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record ExpenseDto(String description, String category, Double amount, LocalDate date) {
+public record ExpenseDto(UUID id, String description, String category, Double amount, LocalDate date) {
     public static ExpenseDto fromExpense(Expense expense) {
-        return new ExpenseDto(expense.getDescription(), expense.getCategory(), expense.getAmount(), expense.getDate());
+        return new ExpenseDto(expense.getId(), expense.getDescription(), expense.getCategory(), expense.getAmount(), expense.getDate());
     }
 }
