686d2509f6679a253719157b618d3940385aa431
Luiz Guilherme <luizgmelo64@gmail.com>
2025-10-26 09:09:56 -0300

    feat: create delete revenue

--- src/main/java/com/luizgmelo/expense_tracker/repositories/RevenueRepository.java
+++ src/main/java/com/luizgmelo/expense_tracker/repositories/RevenueRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RevenueRepository extends JpaRepository<Revenue, UUID> {
     Page<Revenue> findAllByUser(Pageable pageable, User user);
+
+    Optional<Revenue> findByIdAndUser(UUID id, User user);
 }
